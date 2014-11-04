551f07cfcf9d1db4dc9d527f900b4f61d4ac84e3
Konrad Burnik <kburnik@gmail.com>
2014-11-04 11:40:18 +0100

    Added constructor and destructor for MyList

--- Data Structures/List.h
+++ Data Structures/List.h
@@ -22,6 +22,8 @@ namespace Algorithms
     virtual ListElement<T>* getHead() const = 0;
     virtual void setHead(ListElement<T>* new_head) = 0;
     virtual void addFront(T new_value) = 0;
+    virtual void removeFront() = 0;
+    virtual bool empty() = 0;
   };
 
   template <typename T>
@@ -45,7 +47,7 @@ namespace Algorithms
 
     void setNext(ListElement<T>* new_next)
     {
-      next = static_cast<MyListElement<T>*>(new_next);
+      next = dynamic_cast<MyListElement<T>*>(new_next);
     }
 
   private:
@@ -64,15 +66,46 @@ namespace Algorithms
 
     void setHead(ListElement<T>* new_head)
     {
-      head = static_cast<MyListElement<T>*>(new_head);
+      head = dynamic_cast<MyListElement<T>*>(new_head);
     }
 
     void addFront(T new_value)
     {
       MyListElement<T> *node = new MyListElement < T > ;
       node->setValue(new_value);
-      node->setNext(static_cast<ListElement<T>*>(head));
+      node->setNext(dynamic_cast<ListElement<T>*>(head));
       setHead(node);
+    }
+
+    bool empty()
+    {
+      return head == nullptr;
+    }
+
+    void removeFront()
+    {
+     if (!empty())
+     {
+      MyListElement<T> *node = head;
+      setHead(head->getNext());
+      delete node;
+     }
+    }
+
+    void clear()
+    {
+      while(!empty())
+        removeFront();
+    }
+
+    MyList()
+    {
+      head = nullptr;
+    }
+
+    ~MyList()
+    {
+      clear();
     }
 
   private:
